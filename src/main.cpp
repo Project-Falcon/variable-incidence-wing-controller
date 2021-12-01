8fbf68e3926d7f1d37eed20162cfcc210c5fc8b3
Thomas Danvers <tomtomgaming101@gmail.com>
2021-12-01 11:23:58 +0000

    add AccelStepper library and basic code in preparation for stepper motor testing

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,24 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <AccelStepper.h>
   
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+#define dirPin 2
+#define stepPin 3
+#define motorInterfaceType 1
+
+AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
 void setup(void) 
 {
   Serial.begin(9600);
   Serial.println("Orientation Sensor Test"); Serial.println("");
-  
+
+  stepper.setMaxSpeed(1000);  
+  stepper.setAcceleration(500);
+
   /* Initialise the sensor */
   if(!bno.begin())
   {
@@ -25,16 +35,12 @@ void setup(void)
 
 void loop(void) 
 {
-  /* Get a new sensor event */ 
   sensors_event_t event; 
   bno.getEvent(&event);
-  
-  /* Display the floating point data */
-  Serial.print("X: ");
-  Serial.print(event.orientation.x, 4);
-  Serial.print("\tY: ");
-  Serial.print(event.orientation.y, 4);
-  Serial.print("\tZ: ");
-  Serial.print(event.orientation.z, 4);
-  Serial.println("");
+  Serial.print("Y: ");
+  Serial.println(event.orientation.y);
+
+  stepper.moveTo(event.orientation.y);
+  stepper.runToPosition(); // this function is blocking, worth considering 
+  delay(500);
 }

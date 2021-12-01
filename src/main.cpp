78a578d32f204441a4911a0a67b42bfd8c291e97
Thomas Danvers <tomtomgaming101@gmail.com>
2021-12-01 23:01:09 +0000

    sample sensor every 500 steps to prevent stepper motor vibration

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
 #include <AccelStepper.h>
-  
+
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 #define dirPin 2
@@ -12,58 +12,43 @@ Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
 AccelStepper stepper = AccelStepper(motorInterfaceType, stepPin, dirPin);
 
-void setup(void) 
+void setup(void)
 {
   Serial.begin(115200);
-  Serial.println("Orientation Sensor Test"); Serial.println("");
+  Serial.println("Orientation Sensor Test");
+  Serial.println("");
 
-  stepper.setMaxSpeed(1000);  
-  stepper.setAcceleration(500);
+  stepper.setMaxSpeed(1000);
+  stepper.setAcceleration(5000);
 
   /* Initialise the sensor */
-  if(!bno.begin())
+  if (!bno.begin())
   {
     /* There was a problem detecting the BNO055 ... check your connections */
     Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-    while(1);
+    while (1)
+      ;
   }
-  
+
   delay(1000);
-    
+
   bno.setExtCrystalUse(true);
 }
 
-void loop(void) 
+int counter = 0;
+sensors_event_t event;
+
+void loop(void)
 {
-  sensors_event_t event; 
-  bno.getEvent(&event);
-  stepper.moveTo(event.orientation.y * 100);
-  /*
-  Serial.print("Y: ");
-  Serial.print(event.orientation.y);
-  Serial.print(" | distanceToGo: ");
-  Serial.print(stepper.distanceToGo());
-  Serial.print(" | targetPosition: ");
-  Serial.print(stepper.targetPosition());
-  Serial.print(" | currentPosition: ");
-  Serial.print(stepper.currentPosition());
-  Serial.print(" | speed: ");
-  Serial.print(stepper.speed());
-  Serial.println();
-  */
-
-  Serial.println(stepper.distanceToGo());
-
-  
-  
-  
-  if (abs(stepper.distanceToGo()) > 100) {
-    for (int i = 0; i <= 100; i++) {
-      stepper.run();
-    }
-    Serial.println("Stepping");
+  if (counter % 500 == 0)
+  {
+    bno.getEvent(&event);
+    Serial.println(stepper.distanceToGo());
+    stepper.moveTo(event.orientation.y * 100);
   }
-
-  //stepper.moveTo(8000000);
-  //stepper.runToPosition();
+  if (abs(stepper.distanceToGo()) > 150)
+  {
+    stepper.run();
+  }
+  ++counter;
 }
